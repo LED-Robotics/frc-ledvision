41db43dbd4749a2e24c17c3dc8d56b67c6c917e1
Tristan Warder <tristan.warder@outlook.com>
2024-12-23 16:19:38 -0500

    Clean up codebase a bit

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,11 @@ int width = 640;
 int height = 640;
 cs::VideoMode camConfig{cs::VideoMode::PixelFormat::kMJPEG, width, height, 30};
 
+struct Camera {
+  int id = -1;
+  cs::UsbCamera* ref = nullptr;
+};
+
 struct Detection {
     int label = -1;
     double x = 0;
@@ -48,6 +53,7 @@ json detectionJson;
 std::vector<struct Detection> detections = {};
 bool newInference = false;
 bool newFrame = false;
+std::vector<std::string> classes = {"label", "x", "y", "width", "height"};
 
 // AprilTag detection objects
 AprilTagDetector detector{};
@@ -210,6 +216,15 @@ void printCoordinates(int id) {
     }
 }
 
+void debugTagPrint(int id, Transform3d transform) {
+    std::cout << "Tag " << id << " Pose Estimation:" << std::endl;
+    std::cout << "X Off: " << units::foot_t{transform.X()}.value();
+    std::cout << " Y Off: " << units::foot_t{transform.Y()}.value();
+    std::cout << " Z Off: " << units::foot_t{transform.Z()}.value() << std::endl;
+    std::cout << "Rot Off: " << transform.Rotation().ToRotation2d().Degrees().value() << std::endl;
+    std::cout << std::endl;
+}
+
 Pose3d GetTagPose(int id) {
     auto idSearch = tagLayout.GetTagPose(id);
     if(idSearch.has_value()) {  // ensure tag ID exists in our field coordinate system
@@ -225,15 +240,68 @@ Pose3d GetTagPose(int id) {
 std::vector<cs::UsbCamera> initCameras(cs::VideoMode config) {
     CS_Status status = 0;
     std::vector<cs::UsbCamera> cameras{};
-    int number = 0;
     for (const auto& caminfo : cs::EnumerateUsbCameras(&status)) {
         fmt::print("Dev {}: Path {} (Name {})\n", caminfo.dev, caminfo.path, caminfo.name);
         fmt::print("vid {}: pid {}\n", caminfo.vendorId, caminfo.productId);
-        cs::UsbCamera cam{"camera-" + number, caminfo.path};
+        cs::UsbCamera cam{"camera-" + caminfo.dev, caminfo.path};
         cam.SetVideoMode(config);
         cameras.push_back(cam);
     }
     return cameras;
+}
+
+void drawAprilTagBox(cv::Mat frame, const frc::AprilTagDetection* tag) {
+  // Draw boxes around tags for video feed                
+  for(int i = 0; i < 4; i++) {
+      auto point1 = tag->GetCorner(i);
+      int secondIndex = i == 3 ? 0 : i + 1;   // out of bounds adjust for last iteration
+      auto point2 = tag->GetCorner(secondIndex);
+      cv::Point lineStart{(int)point1.x, (int)point1.y};
+      cv::Point lineEnd{(int)point2.x, (int)point2.y};
+      cv::line(frame, lineStart, lineEnd, cv::Scalar(0, 0, 255), 2, cv::LINE_4);
+  }
+}
+
+void constructDetections() {
+  detections.clear();
+  for (auto& detection : detectionJson) {
+  // std::cout << detection << std::endl;
+      for(auto& jsonClass : classes) {
+          if(!detection.contains(jsonClass))
+              continue;
+      }
+      int label = detection["label"];
+      if(label != 0) continue;    // only box notes
+      double x = detection["x"];
+      double y = detection["y"];
+      double width = detection["width"];
+      double height = detection["height"];
+      if(detection.contains("kps")) {
+        std::vector<float> kps;
+        auto points = detection["kps"];
+        for(auto& point : points) {
+          kps.push_back(point["x"]);
+          kps.push_back(point["y"]);
+          kps.push_back(point["s"]);
+          detections.push_back({label, x, y, width, height, kps});
+        }
+      } else {
+        detections.push_back({label, x, y, width, height});
+      }
+  }
+  // std::cout << std::endl;
+  newInference = false;
+}
+
+void drawInferenceBox(cv::Mat frame) {
+  for (auto& detection : detections) {
+      cv::Rect rect(detection.x, detection.y, detection.width, detection.height);
+      cv::rectangle(frame, rect, cv::Scalar(255, 0, 0), 2, cv::LINE_4);
+      for(int i = 0; i < detection.kps.size(); i += 3) {
+        cv::Point center(detection.kps[i], detection.kps[i+1]);
+        cv::circle(frame, center, detection.kps[i+2]*4, cv::Scalar(0, 0, 255), cv::FILLED, cv::LINE_8);
+      }
+  }
 }
 
 int main(int argc, char** argv)
@@ -246,20 +314,32 @@ int main(int argc, char** argv)
     detector.SetQuadThresholdParameters(quadParams);
     tagLayout.SetOrigin(AprilTagFieldLayout::OriginPosition::kBlueAllianceWallRightSide);
     // Initialize cameras
-    auto cameras = initCameras(camConfig);
+    auto rawCameras = initCameras(camConfig);
+    std::vector<Camera> cameras;
     cs::UsbCamera* testCam = nullptr;
-    for(cs::UsbCamera& cam : cameras) {
+    for(cs::UsbCamera& cam : rawCameras) {
         auto info = cam.GetInfo();
         std::cout << "Camera found: " << std::endl;
         std::cout << info.path << ", " << info.name << std::endl;
+        cameras.push_back(Camera{info.dev, &cam});
         if(info.name == "USB2.0 HD UVC WebCam") {
             std::cout << "Test cam found!" << std::endl;
             testCam = &cam;
         }
     }
+
+    for(Camera& cam : cameras) {
+      std::cout << "Cam ID: " << cam.id << std::endl;
+    }
+
+    // NT Initialization
+    auto inst = nt::NetworkTableInstance::GetDefault();
+    inst.SetServerTeam(6722);
+    inst.StartClient4("jetson-client");
+    auto table = inst.GetTable("/jetson");
+
     // Make sure camera was found and initialized
     if(testCam != nullptr) {
-        std::vector<std::string> classes = {"label", "x", "y", "width", "height"};
         cv::Mat frame, grayFrame;
         // Setup camera stream
         cs::CvSink testSink{frc::CameraServer::GetVideo(*testCam)};
@@ -300,59 +380,18 @@ int main(int argc, char** argv)
             }
             newFrame = true;
 
-            // Draw most recent detections
+            // Fill detections array if new detections have been sent
             // This being up-to-date is the inferThread's responsibility
             if(newInference) {
-                detections.clear();
-                for (auto& detection : detectionJson) {
-                // std::cout << detection << std::endl;
-                    for(auto& jsonClass : classes) {
-                        if(!detection.contains(jsonClass))
-                            continue;
-                    }
-                    int label = detection["label"];
-                    if(label != 0) continue;    // only box notes
-                    double x = detection["x"];
-                    double y = detection["y"];
-                    double width = detection["width"];
-                    double height = detection["height"];
-                    cv::Rect rect(x, y, width, height);
-                    cv::rectangle(frame, rect, cv::Scalar(255, 0, 0), 2, cv::LINE_4);
-                    if(detection.contains("kps")) {
-                      std::vector<float> kps;
-                      auto points = detection["kps"];
-                      for(auto& point : points) {
-                        kps.push_back(point["x"]);
-                        kps.push_back(point["y"]);
-                        kps.push_back(point["s"]);
-                        detections.push_back({label, x, y, width, height, kps});
-                      }
-                    } else {
-                      detections.push_back({label, x, y, width, height});
-                    }
-                }
-                // std::cout << std::endl;
-                newInference = false;
+                constructDetections();
             }
 
-            for (auto& detection : detections) {
-                cv::Rect rect(detection.x, detection.y, detection.width, detection.height);
-                cv::rectangle(frame, rect, cv::Scalar(255, 0, 0), 2, cv::LINE_4);
-                for(int i = 0; i < detection.kps.size(); i += 3) {
-                  cv::Point center(detection.kps[i], detection.kps[i+1]);
-                  cv::circle(frame, center, detection.kps[i+2]*4, cv::Scalar(0, 0, 255), cv::FILLED, cv::LINE_8);
-                }
-            }
+            // Draw detections onto frame
+            drawInferenceBox(frame); 
 
             // AprilTag detection requires grayscale
             cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
 
-            // NT Initialization
-            auto inst = nt::NetworkTableInstance::GetDefault();
-            inst.SetServerTeam(6722);
-            inst.StartClient4("jetson-client");
-            auto table = inst.GetTable("/jetson");
-            
             detectedATagCoords.clear(); 
             for(int i = 0; i < kNumbOfDataValuePerTag * tagsRequested.size(); i++) {
                 detectedATagCoords.push_back(69420); // All values of the vector default to 69420 unless detected and requested
@@ -366,20 +405,15 @@ int main(int argc, char** argv)
                 auto findTagTarg = std::find(tagsRequested.begin(), tagsRequested.end(), tag->GetId());
                 // Print relative offset
                     std::cout << "Requested? " << (findTagTarg != tagsRequested.end() ? "Yes" : "No") << std::endl;
-                    std::cout << "Tag " << tag->GetId() << " Pose Estimation:" << std::endl;
-                    std::cout << "X Off: " << units::foot_t{transform.X()}.value();
-                    std::cout << " Y Off: " << units::foot_t{transform.Y()}.value();
-                    std::cout << " Z Off: " << units::foot_t{transform.Z()}.value() << std::endl;
-                    std::cout << "Rot Off: " << transform.Rotation().ToRotation2d().Degrees().value() << std::endl;
-                    std::cout << std::endl;
+                    debugTagPrint(tag->GetId(), transform);
                     // Print estimated field-coordinates of the camera using the tag detection
-                    auto realPose = GetTagPose(tag->GetId());   // actual field coordinate of tag
-                    auto estimatedPose = realPose + transform;  // wow this is so easy
-                    auto estTranslation = estimatedPose.Translation();
-                    std::cout << "Estimated Pose on Field: " << std::endl;
-                    std::cout << "X: " << estTranslation.X().value() << "Y: " << estTranslation.Y().value()<< "Z: " << estTranslation.Z().value() << std::endl;
-                    std::cout << "Rotation: " << estimatedPose.Rotation().ToRotation2d().Degrees().value() << std::endl;
-                    std::cout << std::endl;   
+                    /*auto realPose = GetTagPose(tag->GetId());   // actual field coordinate of tag*/
+                    /*auto estimatedPose = realPose + transform;  // wow this is so easy*/
+                    /*auto estTranslation = estimatedPose.Translation();*/
+                    /*std::cout << "Estimated Pose on Field: " << std::endl;*/
+                    /*std::cout << "X: " << estTranslation.X().value() << "Y: " << estTranslation.Y().value()<< "Z: " << estTranslation.Z().value() << std::endl;*/
+                    /*std::cout << "Rotation: " << estimatedPose.Rotation().ToRotation2d().Degrees().value() << std::endl;*/
+                    /*std::cout << std::endl;   */
                 // Loop through requested vector, ensuring proper index
                 for(int i = 0; i < tagsRequested.size(); i++) { // Sending the estimated field coordinates of the camera 
                     if((i == std::distance(tagsRequested.begin(), findTagTarg)) && detectedATagCoords[kNumbOfDataValuePerTag * i] == 69420) { 
@@ -389,27 +423,20 @@ int main(int argc, char** argv)
                         detectedATagCoords[(i * kNumbOfDataValuePerTag) + 3] = transform.Rotation().ToRotation2d().Degrees().value();
                     }
                 }
-                // Draw boxes around tags for video feed                
-                for(int i = 0; i < 4; i++) {
-                    auto point1 = tag->GetCorner(i);
-                    int secondIndex = i == 3 ? 0 : i + 1;   // out of bounds adjust for last iteration
-                    auto point2 = tag->GetCorner(secondIndex);
-                    cv::Point lineStart{(int)point1.x, (int)point1.y};
-                    cv::Point lineEnd{(int)point2.x, (int)point2.y};
-                    cv::line(frame, lineStart, lineEnd, cv::Scalar(0, 0, 255), 2, cv::LINE_4);
-                }
+                // Draw box on our frame
+                drawAprilTagBox(frame, tag); 
             }
 
             testSource.PutFrame(frame); // post to stream
             // Debug prints for nt sends
             for(int i = 0; i < tagsRequested.size(); i++) {
-                std::cout << "Tag Index " << i << std::endl;
+                /*std::cout << "Tag Index " << i << std::endl;*/
                 for(int j = 0; j < kNumbOfDataValuePerTag; j++) {
                     double val = detectedATagCoords[(i * kNumbOfDataValuePerTag) + j];
-                    std::cout << val << std::endl;
+                    /*std::cout << val << std::endl;*/
                 }
             }
-            table->PutNumberArray("Requested Tag Rel Coords", detectedATagCoords);
+            /*table->PutNumberArray("Requested Tag Rel Coords", detectedATagCoords);*/
         }
     }
 }
