2823bee7806b90d20fa258d6dc5d9b200798ab48
Tristan Warder <tristan.warder@outlook.com>
2025-03-25 20:16:09 -0400

    Updated model, ensured disabled cams do not label ML

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -342,11 +342,11 @@ void Camera::StartLabeller() {
     for(TagDetection& tag : tagDetections) {
       DrawAprilTagBox(labelled, &tag);
     }
-    if(GetMLDetectionMode() == MLMode::Detect) {
+    if(mlEnabled && GetMLDetectionMode() == MLMode::Detect) {
       for(det::DetectObject& det : *boxLabelVector) {
         DrawDetectBox(labelled, det);
       }
-    } else if(GetMLDetectionMode() == MLMode::Pose) {
+    } else if(mlEnabled && GetMLDetectionMode() == MLMode::Pose) {
       for(det::PoseObject& det : *poseLabelVector) {
         DrawPoseBox(labelled, det);
       }
